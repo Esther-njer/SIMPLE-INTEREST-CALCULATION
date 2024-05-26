8ab41ba3631ca4738555f9c28924e82ddb4c818f
Esther-njer <hadasa74n@gmail.com>
2024-05-26 15:34:58 +0300

    Add files via upload

---
+++ office number.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int officenumber;//declaring integer variable
+    printf("Enter your office number\n");
+    scanf("%d",& office number);
+    printf("office number=%d",office number;)
+}
